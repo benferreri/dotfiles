8469ffb582516fb38dd57291628f4d86248041fb
benferreri <benferreri11@gmail.com>
2019-05-26 16:55:05 -0400

    added powerline changing in theme toggle

--- bin/theme-toggle.sh
+++ bin/theme-toggle.sh
@@ -6,9 +6,10 @@ if [ ! -e $TOGGLE ]; then
     touch $TOGGLE
     sed -i 's/\(colors-\).*/\1space/' /home/ben/.config/polybar/config
     wal -i /home/ben/pictures/space.jpg -b 444444
+    sed -i '43,43 s/\("bg":\) \("[a-z]*"\),/\1 "mediumpurple",/' /home/ben/.config/powerline/default.json
 else
     rm $TOGGLE
     sed -i 's/\(colors-\).*/\1autumn/' /home/ben/.config/polybar/config
     wal -i /home/ben/pictures/autumn.jpg -b 444444
-    
+    sed -i '43 s/\("bg":\) \("[a-z]*"\),/\1 "darkred",/' /home/ben/.config/powerline/default.json
 fi
