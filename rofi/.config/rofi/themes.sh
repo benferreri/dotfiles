a212d92707080ad812576e593d95e3f6d7084193
benferreri <benferreri11@gmail.com>
2019-05-27 15:07:52 -0400

    complete overhaul to be compatible with GNU Stow

---
+++ rofi/.config/rofi/themes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z $@ ]; then
+    echo current; ls $HOME/pictures/wallpapers
+else
+    THEMES=$@
+    if [ x"current" = x"${THEMES}" ]; then
+        exit 0
+    elif [ -n "${THEMES}" ]; then
+        theme-change.sh ${THEMES} > /dev/null
+    fi
+fi
---
+++ theming/bin/theme-change.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+WALLPAPERDIR=$HOME/pictures/wallpapers/
+
+# usage: theme-change <IMAGE> <POWERLINE-COLOR>
+if [ $# -ge 1 ]; then
+    IMG=$1
+    IMGNAME=$(echo $IMG | sed 's/\([a-z,A-Z,0-9]*\)\..*/\1/')
+    if [ $# -eq 2 ]; then
+        PWRCOLOR=$2
+    else
+        PWRCOLOR=yellow # default
+    fi
+else
+    # defaults
+    IMG=space.jpg
+    IMGNAME=space
+    PWRCOLOR=mediumpurple
+fi
+
+# change which pywal colors polybar uses (to colors-<imagename> file)
+if [ -e $HOME/.config/polybar/colors-$IMGNAME ]; then
+    sed -i "s/\(colors-\).*/\1$IMGNAME/" $HOME/.config/polybar/config
+else
+    sed -i "s/\(colors-\).*/\1space/" $HOME/.config/polybar/config
+fi
+
+# pywal
+wal -i ${WALLPAPERDIR}${IMG} -b 444444
+
+# change powerline color
+sed -i "43 s/\(\"bg\":\) \(\"[^,]*\"\),/\1 \"$PWRCOLOR\",/" $HOME/.config/powerline/default.json
+
+# reset dunst; it breaks for some reason after change
+pkill dunst
